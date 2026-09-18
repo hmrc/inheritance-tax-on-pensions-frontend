2ca963ddb142559d69c23b52cbd45ade4336a710
kberec <26416940+kberec@users.noreply.github.com>
2026-09-18 13:09:15 +0100

    [IHTP-586] Refactor for continue page

--- app/utils/CheckYourAnswersHelper.scala
+++ app/utils/CheckYourAnswersHelper.scala
@@ -16,12 +16,21 @@
 
 package utils
 
-import models.JourneyRole.{PrIndividual, PrOrganisation}
+import models.JourneyRole._
+import viewmodels.implicits._
 import play.api.mvc.Call
 import pages._
-import controllers.routes
 import models.SchemeId.Srn
-import models.{NormalMode, PrType, UserAnswers}
+import uk.gov.hmrc.govukfrontend.views.Aliases.Actions
+import viewmodels.govuk.all.{ActionItemViewModel, CardViewModel, SummaryListViewModel}
+import models.beneficiary.{Beneficiaries, BeneficiaryType}
+import models._
+import pages.beneficiary.BeneficiariesPage
+import play.api.i18n.{Lang, Messages, MessagesApi}
+import viewmodels.CheckAnswers.beneficiary._
+import controllers.routes
+import viewmodels.CheckAnswers._
+import viewmodels.govuk.summarylist._
 
 object CheckYourAnswersHelper {
 
@@ -30,9 +39,110 @@ object CheckYourAnswersHelper {
     call: Call
   )
 
-  def findPageToContinue(answers: UserAnswers, srn: Srn): Option[Call] = {
-    // Reference and deceased name are needed to reach this point
-    lazy val continuationPages = Seq(
+  def buildSummaryLists(userAnswers: UserAnswers, srn: Srn, countryName: String => String, messagesApi: MessagesApi)(
+    implicit messages: Messages
+  ): CheckYourAnswersSummaryLists = {
+    val deceasedDetailsSummaryList = SummaryListViewModel(
+      rows = Seq(
+        InheritanceTaxReferenceSummary.row(srn, userAnswers),
+        NameOfDeceasedSummary.row(srn, userAnswers),
+        HasNinoSummary.row(srn, userAnswers),
+        NinoSummary.row(srn, userAnswers),
+        NoNinoReasonSummary.row(srn, userAnswers),
+        BirthDeathDatesSummary.row(srn, userAnswers)
+      ).flatten
+    )
+    val prDetailsSummaryList = SummaryListViewModel(
+      rows = Seq(
+        PrTypeSummary.row(srn, userAnswers),
+        PrIndividualNameSummary.row(srn, userAnswers),
+        PrOrganisationNameSummary.row(srn, userAnswers),
+        PrOrganisationPrNameSummary.row(srn, userAnswers),
+        PrIndividualCountrySummary.row(srn, userAnswers, countryName),
+        PrIndividualAddressSummary.row(srn, userAnswers),
+        PrOrganisationCountrySummary.row(srn, userAnswers, countryName),
+        PrOrganisationAddressSummary.row(srn, userAnswers)
+      ).flatten
+    )
+    val paymentNoticeDetailsSummaryList = SummaryListViewModel(
+      rows = Seq(
+        DidPrSubmitSummary.row(srn, userAnswers),
+        PaymentNoticeDateSummary.row(srn, userAnswers),
+        AreBeneficiariesKnownSummary.row(srn, userAnswers),
+        IhtPayableSummary.row(srn, userAnswers),
+        NumberOfBeneficiariesSummary.row(srn, userAnswers)
+      ).flatten
+    )
+    val beneficiaryList = userAnswers
+      .get[Beneficiaries](BeneficiariesPage())
+      .map(
+        _.beneficiaries.zipWithIndex
+          .map { case (_, index) =>
+            SummaryListViewModel(
+              rows = Seq(
+                BeneficiaryTypeSummary.row(srn, index, userAnswers),
+                BeneficiaryIndividualNameSummary.row(srn, index, userAnswers),
+                viewmodels.CheckAnswers.beneficiary.BeneficiaryTrustNameSummary.row(
+                  srn,
+                  index,
+                  userAnswers
+                ),
+                BeneficiaryHasNinoSummary.row(srn, index, userAnswers)
+              ).flatten
+            ).withCard(
+              CardViewModel(
+                messagesApi("checkYourAnswers.beneficiary.details.card.title", index + 1)(using
+                  Lang.defaultLang
+                ),
+                2,
+                Some(
+                  Actions(
+                    items = Seq(
+                      ActionItemViewModel(
+                        "site.remove",
+                        controllers.beneficiary.routes.RemoveBeneficiaryController
+                          .onPageLoad(srn, CheckMode, index)
+                          .url
+                      )
+                        .withVisuallyHiddenText(
+                          messagesApi(
+                            "checkYourAnswers.beneficiary.details.card.remove.hidden",
+                            BeneficiaryNameHelper.fromUserAnswers(userAnswers, index).getOrElse(index)
+                          )(using
+                            Lang.defaultLang
+                          )
+                        )
+                    )
+                  )
+                )
+              )
+            )
+          }
+      )
+      .getOrElse(List())
+
+    CheckYourAnswersSummaryLists(
+      deceasedDetailsSummaryList,
+      prDetailsSummaryList,
+      paymentNoticeDetailsSummaryList,
+      beneficiaryList
+    )
+  }
+
+  def findPageToContinue(userAnswers: UserAnswers, srn: Srn): Option[Call] = {
+    val allPages = getDeceasedPages(srn) :++ getPrPages(srn) :++ getBeneficiariesPages(userAnswers, srn)
+    val found = allPages
+      .find(_.isUnanswered(userAnswers))
+      .map(_.call)
+    found
+  }
+
+  private def getDeceasedPages(srn: Srn) =
+    Seq(
+      ContinuationPage(
+        answers => answers.get(IndividualNamePage(Deceased)).isEmpty,
+        routes.IndividualNameController.onPageLoad(srn, NormalMode, Deceased)
+      ),
       ContinuationPage(
         answers => answers.get(HasNinoPage).isEmpty,
         routes.HasNinoController.onPageLoad(srn, NormalMode)
@@ -48,7 +158,11 @@ object CheckYourAnswersHelper {
       ContinuationPage(
         answers => answers.get(BirthDeathDatesPage).isEmpty,
         routes.BirthDeathDatesController.onPageLoad(srn, NormalMode)
-      ),
+      )
+    )
+
+  private def getPrPages(srn: Srn) =
+    Seq(
       ContinuationPage(
         answers => answers.get(PrTypePage).isEmpty,
         routes.PrTypeController.onPageLoad(srn, NormalMode)
@@ -83,44 +197,47 @@ object CheckYourAnswersHelper {
         routes.DidPrSubmitController.onPageLoad(srn, NormalMode)
       ),
       ContinuationPage(
+        answers => answers.get(DidPrSubmitPage).get && answers.get(AreBeneficiariesKnownPage).isEmpty,
+        routes.AreBeneficiariesKnownController.onPageLoad(srn, NormalMode)
+      ),
+      ContinuationPage(
         answers => answers.get(PaymentNoticeDatePage).isEmpty,
         routes.PaymentNoticeDateController.onPageLoad(srn, NormalMode)
       ),
       ContinuationPage(
-        answers => answers.get(DidPrSubmitPage).get && answers.get(AreBeneficiariesKnownPage).isEmpty,
-        routes.AreBeneficiariesKnownController.onPageLoad(srn, NormalMode)
+        answers =>
+          answers.get(AreBeneficiariesKnownPage).getOrElse(false) &&
+            answers.get(pages.beneficiary.BeneficiaryTypePage(0)).isEmpty,
+        controllers.beneficiary.routes.BeneficiaryTypeController.onPageLoad(srn, 0, NormalMode)
       )
     )
 
+  private def getBeneficiaryPages(srn: Srn, i: Int) =
+    Seq(
+      ContinuationPage(
+        answers =>
+          answers.get(pages.beneficiary.BeneficiaryTypePage(i)).contains(BeneficiaryType.Individual) &&
+            answers.get(pages.beneficiary.BeneficiaryNamePage(i, BeneficiaryIndividual)).isEmpty,
+        controllers.beneficiary.routes.BeneficiaryNameController.onPageLoad(srn, NormalMode, i)
+      ),
+      ContinuationPage(
+        answers =>
+          answers.get(pages.beneficiary.BeneficiaryTypePage(i)).contains(BeneficiaryType.Trust) &&
+            answers.get(pages.beneficiary.BeneficiaryTrustNamePage(i)).isEmpty,
+        controllers.beneficiary.routes.BeneficiaryTrustNameController.onPageLoad(srn, i, NormalMode)
+      ),
+      ContinuationPage(
+        answers =>
+          answers.get(pages.beneficiary.BeneficiaryTypePage(i)).contains(BeneficiaryType.Individual) &&
+            answers.get(pages.beneficiary.BeneficiaryHasNinoPage(i)).isEmpty,
+        controllers.beneficiary.routes.BeneficiaryHasNinoController.onPageLoad(srn, i, NormalMode)
+      )
+    )
+
+  private def getBeneficiariesPages(answers: UserAnswers, srn: Srn) = {
     val numberOfBeneficiaries =
       answers.get(pages.beneficiary.BeneficiariesPage()).map(_.beneficiaries.size).getOrElse(0)
 
-    if (numberOfBeneficiaries > 0) {
-      for (i <- 1 to numberOfBeneficiaries)
-        continuationPages :++ Seq(
-          ContinuationPage(
-            answers => answers.get(pages.beneficiary.BeneficiaryTypePage(i)).isEmpty,
-            controllers.beneficiary.routes.BeneficiaryTypeController.onPageLoad(srn, i, NormalMode)
-          ),
-          ContinuationPage(
-            answers => answers.get(pages.beneficiary.BeneficiaryNamePage(i, PrIndividual)).isEmpty,
-            controllers.beneficiary.routes.BeneficiaryNameController.onPageLoad(srn, NormalMode, i)
-          ),
-          ContinuationPage(
-            answers => answers.get(pages.beneficiary.BeneficiaryTrustNamePage(i)).isEmpty,
-            controllers.beneficiary.routes.BeneficiaryTrustNameController.onPageLoad(srn, i, NormalMode)
-          ),
-          ContinuationPage(
-            answers => answers.get(pages.beneficiary.BeneficiaryHasNinoPage(i)).isEmpty,
-            controllers.beneficiary.routes.BeneficiaryHasNinoController.onPageLoad(srn, i, NormalMode)
-          ),
-          ContinuationPage(
-            answers => answers.get(pages.beneficiary.BeneficiaryNamePage(i, PrOrganisation)).isEmpty,
-            controllers.beneficiary.routes.BeneficiaryNameController.onPageLoad(srn, NormalMode, i)
-          )
-        )
-    }
-
-    continuationPages.find(_.isUnanswered(answers)).map(_.call)
+    (0 until numberOfBeneficiaries).flatMap(i => getBeneficiaryPages(srn, i))
   }
 }
